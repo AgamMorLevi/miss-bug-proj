55370c4b3b67d364bed92fa750d9bb60ec900748
AgamMorLevi <agammor96@gmail.com>
2025-04-23 19:33:28 +0300

    add bug.service.remote file

---
+++ public/services/bug.service.remote.js
@@ -0,0 +1,51 @@
+const BASE_URL = '/api/bug/'
+
+export const bugService = {
+    query,
+    getById,
+    save,
+    remove,
+    getDefaultFilter
+}
+
+function query(filterBy) {
+    return axios.get(BASE_URL)
+    .then (res => res.data)
+    .then(bugs => {
+
+        if (filterBy.txt) {
+            const regExp = new RegExp(filterBy.txt, 'i')
+            bugs = bugs.filter(bug => regExp.test(bug.title))
+        }
+
+        if (filterBy.minSeverity) {
+            bugs = bugs.filter(bug => bug.severity >= filterBy.minSeverity)
+        }
+
+        return bugs
+    })
+}
+
+function getById(bugId) {
+    return axios.get( BASE_URL + bugId)
+    .then(res => res.data)  
+}
+
+function remove(bugId) {
+    const url = BASE_URL + bugId +'/remove'
+    return axios.get(url)
+}
+
+function save(bug) {
+    var queryparse = `title=${bug.title}&description=${bug.description}&severity=${bug.severity}`
+    const url = BASE_URL + 'save?' + queryparse
+    if (bug._id)  queryparse += `&_id=${bug._id}`
+    return axios.get(url)
+    .then(res => res.data)
+
+}
+
+
+function getDefaultFilter() {
+    return { txt: '', minSeverity: 0 }
+}
